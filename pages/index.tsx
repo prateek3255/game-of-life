3e55c21cbae6a02616bd7fc4fc75c3095f0e0136
Prateek Surana <prateeksurana3255@gmail.com>
2021-08-15 00:24:40 +0530

    :sparkles: Add initial layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,58 @@
-import Head from 'next/head'
+import React from "react";
+
+
+const Cell = ({ alive }: {alive: boolean}) => {
+  return (
+    <div role="button" className={`h-8 w-8 border-[1px] border-black ${alive ? 'bg-black' : 'bg-white'}`} />
+  )
+}
+
+type CellState = boolean[][];
+
+type Action = { type: 'initialize', rows: number; columns: number };
+
+function reducer(state: CellState, action: Action): CellState {
+  switch (action.type) {
+    case 'initialize':
+      return new Array(action.columns).fill(false).map(i => new Array(action.rows).fill(false));
+    default:
+    return state;
+  }
+}
+
 
 export default function Home() {
+
+  const [cells, dispatch] = React.useReducer(reducer, []);
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+
+    const columns = Math.floor((window.innerHeight - 200) / 32);
+    const rows = Math.floor(window.innerWidth / 32);
+    dispatch({ type: 'initialize', rows, columns });
+
+  }, []);
+
+  if (!isMounted) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="h-full w-full flex flex-col justify-center items-center">
+      <div className="min-h-[100px] text-center flex items-center">
+          <h1 className="font-sans font-black text-4xl sm:text-5xl">Game of life</h1>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+      <div className="flex flex-col">
+        {cells.map((row, i) => (
+          <div key={i} className="flex flex-row">
+            {row.map((cell, j) => (
+              <Cell key={`${i}-${j}`} alive={cell} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>  
   )
 }
