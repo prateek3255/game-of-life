883747b52a7b343d6623cf7cda2691b494096f68
Prateek Surana <prateeksurana3255@gmail.com>
2021-08-21 12:28:31 +0530

    🚨  Format file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,49 @@
 import React from "react";
 import debounce from "lodash/debounce";
 
-const getCellSize = () => typeof window !== "undefined" && window.innerWidth >= 500 ? 24 : 20;
+const getCellSize = () =>
+  typeof window !== "undefined" && window.innerWidth >= 500 ? 24 : 20;
 
-const Cell = React.memo(({ alive, handleClick, row, col }: { alive: boolean, handleClick: (row: number, col: number) => void, row: number, col: number }) => {
-  const onClick = () => {
-    handleClick(row, col);
+const Cell = React.memo(
+  ({
+    alive,
+    handleClick,
+    row,
+    col,
+  }: {
+    alive: boolean;
+    handleClick: (row: number, col: number) => void;
+    row: number;
+    col: number;
+  }) => {
+    const onClick = () => {
+      handleClick(row, col);
+    };
+    return (
+      <button
+        onClick={onClick}
+        style={{ height: getCellSize(), width: getCellSize() }}
+        className={`border-[1px] border-blue-200 ${
+          alive ? "bg-blue-400" : "bg-white"
+        }`}
+      />
+    );
   }
-  return (
-    <button onClick={onClick} style={{ height: getCellSize(), width: getCellSize() }} className={`border-[1px] border-gray-300 ${alive ? 'bg-black' : 'bg-white'}`} />
-  )
-});
+);
 
 type CellState = number[][];
 
-type Action = | { type: 'initialize', random?: boolean } 
-              | { type: 'cell_clicked', row: number, col: number } 
-              | { type: 'generate_next_state' };
+type Action =
+  | { type: "initialize"; random?: boolean }
+  | { type: "cell_clicked"; row: number; col: number }
+  | { type: "generate_next_state" };
 
 function generateBoard(random?: boolean): CellState {
-  const rows = Math.floor(Math.min((window.innerHeight - 200), 1200) / getCellSize());
+  const rows = Math.floor(
+    Math.min(window.innerHeight - 200, 1200) / getCellSize()
+  );
   const columns = Math.floor(Math.min(window.innerWidth, 1200) / getCellSize());
-  const state = new Array(rows).fill(0).map(i => new Array(columns).fill(0));
+  const state = new Array(rows).fill(0).map((i) => new Array(columns).fill(0));
 
   if (random) {
     // Create a random board where each cell has
@@ -36,13 +58,19 @@ function generateBoard(random?: boolean): CellState {
   return state;
 }
 
-function getAliveNeighbours(state: CellState, x: number, y: number, rows: number, cols: number): number {
+function getAliveNeighbours(
+  state: CellState,
+  x: number,
+  y: number,
+  rows: number,
+  cols: number
+): number {
   let sum = 0;
-  for(let i = -1; i < 2; i++) {
-    for(let j = -1; j < 2; j++) {
-      // Use the modulus operator to handles the 
-      // edge cases where cells are on the  edge 
-      // of the board, it will wrap the cell around 
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      // Use the modulus operator to handles the
+      // edge cases where cells are on the  edge
+      // of the board, it will wrap the cell around
       // the board. Inspired by the coding train's solution
       // https://www.youtube.com/watch?v=FWSR_7kZuYg
       const xPos = (x + i + rows) % rows;
@@ -58,103 +86,120 @@ function getAliveNeighbours(state: CellState, x: number, y: number, rows: number
 
 function reducer(state: CellState, action: Action): CellState {
   switch (action.type) {
-    case 'initialize':
+    case "initialize":
       return generateBoard(action.random);
-    case 'cell_clicked':
+    case "cell_clicked":
       const newCellState = state[action.row][action.col] === 0 ? 1 : 0;
       const updatedState = [
         ...state.slice(0, action.row),
-        [...state[action.row].slice(0, action.col), newCellState, ...state[action.row].slice(action.col + 1)],
-        ...state.slice(action.row + 1)
-      ]
+        [
+          ...state[action.row].slice(0, action.col),
+          newCellState,
+          ...state[action.row].slice(action.col + 1),
+        ],
+        ...state.slice(action.row + 1),
+      ];
       return updatedState;
-    case 'generate_next_state':
+    case "generate_next_state":
       const nextState = generateBoard();
       const totalRows = state.length;
       const totalCols = state[0].length;
       // Using for loop here for simplicity and readability
       // can change later if a more readable way is found
-      for(let i = 0; i < totalRows; i++) {
-        for(let j = 0; j < totalCols; j++) {
+      for (let i = 0; i < totalRows; i++) {
+        for (let j = 0; j < totalCols; j++) {
           const cellState = state[i][j];
-          const aliveNeighbours = getAliveNeighbours(state, i, j, totalRows, totalCols);
+          const aliveNeighbours = getAliveNeighbours(
+            state,
+            i,
+            j,
+            totalRows,
+            totalCols
+          );
 
-          if(cellState === 0 && aliveNeighbours === 3) {
+          if (cellState === 0 && aliveNeighbours === 3) {
             nextState[i][j] = 1;
-          } else if(cellState === 1 && (aliveNeighbours < 2 || aliveNeighbours > 3)) {
+          } else if (
+            cellState === 1 &&
+            (aliveNeighbours < 2 || aliveNeighbours > 3)
+          ) {
             nextState[i][j] = 0;
           } else {
             nextState[i][j] = cellState;
-          } 
-
+          }
         }
       }
       return nextState;
     default:
-    return state;
+      return state;
   }
 }
 
-
 export default function Home() {
-
   const [cells, dispatch] = React.useReducer(reducer, []);
   const [isMounted, setIsMounted] = React.useState(false);
   const intervalRef = React.useRef<NodeJS.Timeout | undefined>();
 
   React.useEffect(() => {
     const handleResize = () => {
-      dispatch({ type: 'initialize' });
-    }
+      dispatch({ type: "initialize" });
+    };
     setIsMounted(true);
     handleResize();
-    window.addEventListener('resize', debounce(handleResize, 200));
+    window.addEventListener("resize", debounce(handleResize, 200));
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-    }
+      if (intervalRef.current !== undefined) {
+        clearInterval(intervalRef.current);
+      }
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   const handleCellClick = React.useCallback((row: number, col: number) => {
-    dispatch({ type: 'cell_clicked', row, col });
-  },[])
+    dispatch({ type: "cell_clicked", row, col });
+  }, []);
 
   const play = () => {
     intervalRef.current = setInterval(() => {
-      dispatch({ type: 'generate_next_state' });
-    }, 100)
-  }
+      dispatch({ type: "generate_next_state" });
+    }, 100);
+  };
 
   const reset = (random = false) => {
-    dispatch({ type: 'initialize', random });
+    dispatch({ type: "initialize", random });
     if (intervalRef.current !== undefined) {
       clearInterval(intervalRef.current);
     }
-  }
-
-  if (!isMounted) {
-    return <div>Loading...</div>
-  }
+  };
 
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center">
-      <div className="min-h-[100px] text-center flex items-center">
-          <h1 className="font-sans font-black text-4xl sm:text-5xl">Game of life</h1>
-        </div>
-      <div className="flex flex-col">
+    <div className="h-screen w-full flex flex-col justify-center items-center bg-blue-100">
+      <div className="min-h-[80px] sm:min-h-[100px] text-center flex items-center">
+        <h1 className="font-sans font-black text-4xl sm:text-5xl text-blue-500">
+          Game of Life
+        </h1>
+      </div>
+      <div className="flex flex-col border-[1px] border-blue-200">
         {cells.map((row, i) => (
           <div key={i} className="flex flex-row">
             {row.map((cell, j) => (
-              <Cell key={`${i}-${j}`} row={i} col={j} alive={cell === 1} handleClick={handleCellClick} />
+              <Cell
+                key={`${i}-${j}`}
+                row={i}
+                col={j}
+                alive={cell === 1}
+                handleClick={handleCellClick}
+              />
             ))}
           </div>
         ))}
       </div>
-      <div className="flex">
+      <div className="flex h-full">
         <button onClick={play}>Start</button>
         <button onClick={() => reset()}>Reset</button>
         <button onClick={() => reset(true)}>Random</button>
       </div>
-    </div>  
-  )
+    </div>
+  );
 }
